2512fe677b7cdff9efbd77d7b4e67eafd27765ad
David Veytsman <david.veytsman@gmail.com>
2024-02-21 14:49:11 -0500

    - formatting the code - adding more detail to the README document

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Sample quick code for hangman game in python using the terminal
+# Hangman
+
+## About
+
+This is a short program implementing a hangman game through the terminal
+
+### Game Play
+
+from the terminal run the command used to run python scripts + hangman_game.py.
+In my case the command is `python3 hangman_game.py`.
+
+### Further information
+
+You can edit the words.txt document locally to add or change the word selection.
+
+### Possible further additions TBD
+
+- adding a game reset option
+- adding a loop to restart the game
+- creating a better visual interfaced
--- hangman_game.py
+++ hangman_game.py
@@ -11,20 +11,23 @@ if play == "Y":
 else:
     game_on = False
 
-# choose a word
+# letters for validation
 alphabet = list("abcdefghijklmnopqrstuvwxyz")
+
+# importing words and formatting them and grabbing the game word
 with open('words.txt', 'r') as file:
     words = file.readlines()
     word = random.choice(words)[:-1]
-    #TODO: remove the hint
-    print(f"psst the word is {word}")
+
     number_of_guesses = 0
-# set up the display
+
+    # set up the display
     display = []
 
     for letter in word:
         display.append("_")
 
+    # game loop starts and loops until the number of guesses run out or until the player wins
     while game_on:
         if number_of_guesses >= 6:
             game_on = False
@@ -34,18 +37,24 @@ with open('words.txt', 'r') as file:
             game_on = False
             print("you won")
             break
-
+        
+        # grabs the users guess. if the letter is in the word replace those spaces in the display
         guess = input("Pick a letter\n").lower()
         if guess in word and guess not in display:
             for i in range(len(word)):
                 if word[i] == guess:
                     display[i] = word[i]
+        # if the letter has been guessed, ask again
         elif guess in display:
             print("you've already guessed this letter, please try again.")
+        # if the guess isn't a valid choice, ask for another guess
         elif guess not in alphabet:
             print("not a valid guess, try again")
+        # otherwise the guess isn't valid
+        # increment the number of wrong guesses
+        # print out the visual representation
         else:
             number_of_guesses += 1
             print(hangman_state[number_of_guesses])
-
+        # after each loop print the state of the guesses
         print(display)
